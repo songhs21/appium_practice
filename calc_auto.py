297f32415e76e8181dc00b614df8eb43263f3001
songhs21 <108976684+songhs21@users.noreply.github.com>
2024-03-26 14:15:08 +0900

    Update calc_auto.py
    15자리 초과하면 나오는 스낵바 체크 코드 추가

--- calc_auto.py
+++ calc_auto.py
@@ -26,12 +26,13 @@ class SampleTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
+    #더하기
     def test_plus(self):
         self.tact("9")
         self.tact("더하기")
         self.tact("7")
         self.tact("계산")
-
+    #빼기
     def test_minus(self):
         self.tact("8")
         self.tact("5")
@@ -39,14 +40,14 @@ class SampleTest(unittest.TestCase):
         self.tact("4")
         self.tact("6")
         self.tact("계산")
-
+    #나누기
     def test_devision(self):
         self.tact("3")
         self.tact("0")
         self.tact("나누기")
         self.tact("2")
         self.tact("계산")
-
+    #곱하기
     def test_multi(self):
         self.tact("1")
         self.tact("소수점")
@@ -54,7 +55,29 @@ class SampleTest(unittest.TestCase):
         self.tact("곱하기")
         self.tact("4")
         self.tact("계산")
-    
+    # 15자리 초과 체크
+    def test_numover(self):
+        self.tact("1")
+        self.tact("2")
+        self.tact("3")
+        self.tact("4")
+        self.tact("5")
+        self.tact("6")
+        self.tact("7")
+        self.tact("8")
+        self.tact("9")
+        self.tact("0")
+        self.tact("1")
+        self.tact("2")
+        self.tact("3")
+        self.tact("4")
+        self.tact("5")
+        self.tact("6")
+        chk = self.driver.find_element(by=MobileBy.ID, value="com.sec.android.app.popupcalculator:id/snackbar_text").text
+        if chk == "15자리까지 입력할 수 있어요.":
+            print("자리수 제한 체크 ok")
+        else:
+            print("실패")
 
 
 
