71d6a08c4916b91d971fac0c4b4483511841c322
songhs21 <108976684+songhs21@users.noreply.github.com>
2024-03-27 11:15:02 +0900

    Update calc_auto.py
    테스트 리포트 생성, 자리수 체크 반복문으로 개선, setUp/tearDown을 classmethod로 변경

--- calc_auto.py
+++ calc_auto.py
@@ -1,4 +1,5 @@
 import unittest
+import HtmlTestRunner
 from time import sleep
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
@@ -11,9 +12,9 @@ class SampleTest(unittest.TestCase):
         sleep(0.1)
         if act == "계산":
             self.driver.find_element(by=MobileBy.ACCESSIBILITY_ID, value="초기화").click()
-
-    def setUp(self) :
-            self.driver = webdriver.Remote(
+    @classmethod
+    def setUpClass(cls) :
+            cls.driver = webdriver.Remote(
                 command_executor= "http://127.0.0.1:4723/wd/hub",
                 desired_capabilities={
                     "deviceName": "R3CW70RDK9F",
@@ -22,8 +23,8 @@ class SampleTest(unittest.TestCase):
                     "appium:appActivity" : "com.sec.android.app.popupcalculator.Calculator"
                 }
             )
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.driver.quit()
 
     #더하기
@@ -57,30 +58,23 @@ class SampleTest(unittest.TestCase):
         self.tact("계산")
     # 15자리 초과 체크
     def test_numover(self):
-        self.tact("1")
-        self.tact("2")
-        self.tact("3")
-        self.tact("4")
-        self.tact("5")
-        self.tact("6")
-        self.tact("7")
-        self.tact("8")
-        self.tact("9")
-        self.tact("0")
-        self.tact("1")
-        self.tact("2")
-        self.tact("3")
-        self.tact("4")
-        self.tact("5")
-        self.tact("6")
+        for i in range(1,17):
+            if i>=10:
+                i= i-10
+            self.tact(str(i))
         chk = self.driver.find_element(by=MobileBy.ID, value="com.sec.android.app.popupcalculator:id/snackbar_text").text
+        self.tact("초기화")
         if chk == "15자리까지 입력할 수 있어요.":
-            print("자리수 제한 체크 ok")
+            print("digi check ok")
         else:
-            print("실패")
-
+            print("digi check fail")
 
 
 if __name__ == "__main__" :
     suite = unittest.TestLoader().loadTestsFromTestCase(SampleTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+#테스트 리포트 작성용 실행 코드
+# if __name__ == "__main__" :
+#     suite = unittest.TestLoader().loadTestsFromTestCase(SampleTest)
+#     HtmlTestRunner.HTMLTestRunner(output="samCaclTestReport", report_name="samCaclTestReport", report_title="samCaclTestReport", combine_reports=True).run(suite)
